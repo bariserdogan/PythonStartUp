5b0d6d06e4ddff3f8f4aba3ce2ea0109109c341a
bariserdogan <brserdgn@gmail.com>
2017-10-25 10:23:16 +0300

    sys.info

--- seven.py
+++ seven.py
@@ -3,32 +3,52 @@
 
 import random
 import string
+import sys
 
-liste=[]
-i=0
-while i<7:
-    k=raw_input("İsim giriniz:")
-    liste.append(k)
-    i=i+1
+##liste=[]
+##i=0
+##while i<7:
+##    k=raw_input("İsim giriniz:")
+##    liste.append(k)
+##    i=i+1
+##
+##print("Orjinal Liste")
+##for n in liste:
+##    print(n)
+##
+##print("{{{{{{{{{{{{===========}}}}}}}}}}}}}}}}}}}}}}}}")
+##
+##newlist=[]
+##for i in range(len(liste)):
+##    temp = random.choice(liste)
+##    if temp not in newlist:
+##        newlist.append(temp)
+##    else:
+##        print("Ooops...!!! Aynı öğeler tekrar tekrar çekilemez")
+##     
+##
+##print("Yeni oluşturulmuş liste:\n********************")
+##for n in newlist:
+##    print(n)
 
-print("Orjinal Liste")
-for n in liste:
-    print(n)
 
-print("{{{{{{{{{{{{===========}}}}}}}}}}}}}}}}}}}}}}}}")
+##Sistem bilgilerini Gösterme
 
-newlist=[]
-for i in range(len(liste)):
-    temp = random.choice(liste)
-    if temp not in newlist:
-        newlist.append(temp)
-    else:
-        print("Ooops...!!! Aynı öğeler tekrar tekrar çekilemez")
-     
+##def show_sys_info():
+##    print("\nSistemde kurulu python'un")
+##    print("\tAna sürüm numarası: {}".format(sys.version_info.major
+##                                            ))
+##    print("\tAlt sürüm numarası: {}".format(sys.version_info.minor
+##                                            ))
+##    print("\tEn alt sürüm numarası: {}".format(sys.version_info.micro
+##                                               ))
+##
+##    print("\nKullanılan işletim sisteminin:")
+##    print("\tAdı: {}".format(sys.platform))
+##    
+##
+##show_sys_info()
 
-print("Yeni oluşturulmuş liste:\n********************")
-for n in newlist:
-    print(n)
 
 
 
